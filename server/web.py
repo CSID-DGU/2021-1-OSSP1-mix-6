593a06c385d60e735f2dfb7f5c74748b4cdc84d8
Dongw1126 <topblade71@naver.com>
2021-04-19 17:32:01 +0900

    채점서버 수정

--- server/web.py
+++ server/web.py
@@ -4,6 +4,7 @@ import os
 app = Flask(__name__)
 print("server start")
 
+
 @app.route('/result', methods=['POST'])
 def call_judge(code=""):
     usr_src = "empty"
@@ -23,6 +24,32 @@ def call_judge(code=""):
         os.waitpid(pid, 0)
         f_out = open('output.txt', 'r')
         result = "실행 결과 :\n" + f_out.read()
+
+    return result
+
+
+@app.route('/vscode', methods=['POST'])
+def call_judge_vscode(code=""):
+    usr_src = "empty"
+    result = "empty"
+
+    if request.method == 'POST':
+        req = request.get_json()
+        usr_src = req['code']
+
+        f_in = open('usr_code.cpp', 'w')
+        f_in.write(usr_src)
+        f_in.close()
+
+        pid = os.fork()
+        if pid == 0:
+            os.execl("/usr/local/bin/python3", "python3", "/app/judge.py")
+
+        os.waitpid(pid, 0)
+        f_out = open('output.txt', 'r')
+        result = "result :\n" + f_out.read()
+
+        return jsonify(result)
 
     return result
 
