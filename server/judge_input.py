27fb48546556ad4e760d6915c1317da2b7fa78c7
Dongw1126 <topblade71@naver.com>
2021-05-05 04:09:07 +0900

    여러 입력에 대해 테스트 할 수 있게 변경

--- server/judge_input.py
+++ server/judge_input.py
@@ -4,28 +4,49 @@ from settings import *
 
 print("start input judge")
 error = 0
-iter = 1
+iter = 3
 
+pid_list = []
+for i in range(iter):
+    pid = os.fork()
+    pid_list.append(pid)
+    if pid == 0:
+        filepath = INPUT_DIR_PATH + "input"+str(i)+".txt"
+        f_in = os.open(filepath, os.O_RDWR | os.O_CREAT)
+        fd = os.fdopen(f_in, "w")
+    	
+        stdin_origin = sys.stdin
+        print("Input"+str(i)+" : " + open(filepath, 'r').read())
+        os.dup2(fd.fileno(), sys.stdin.fileno())
+        os.close(fd.fileno())
+        os.execl(OBJ_FILE_PATH, OBJ_FILE_PATH)
+
+for i in range(iter):
+    info = os.waitpid(pid_list[i], 0)
+    exit_code = os.WEXITSTATUS(info[1])
+    if exit_code != 0:
+        error += 1
+'''
 pid = os.fork()
-
+    
 if pid == 0:
     for i in range(iter):
-    	filepath = INPUT_DIR_PATH + "input1.txt"
-    	f_in = os.open(filepath, os.O_RDWR | os.O_CREAT)
-    	fd = os.fdopen(f_in, "w")
+        filepath = INPUT_DIR_PATH + "input"+str(i)+".txt"
+        f_in = os.open(filepath, os.O_RDWR | os.O_CREAT)
+        fd = os.fdopen(f_in, "w")
     	
-    	stdin_origin = sys.stdin
-
-    	os.dup2(fd.fileno(), sys.stdin.fileno())
-    	os.close(fd.fileno())
-    	os.execl(OBJ_FILE_PATH, OBJ_FILE_PATH)
+        stdin_origin = sys.stdin
+        print("Input"+str(i)+" : " + open(filepath, 'r').read())
+        os.dup2(fd.fileno(), sys.stdin.fileno())
+        os.close(fd.fileno())
+        os.execl(OBJ_FILE_PATH, OBJ_FILE_PATH)
 else:
     for i in range(iter):
-    	info = os.waitpid(pid, 0)
-    	exit_code = os.WEXITSTATUS(info[1])
-    	if exit_code != 0:
-    	    error += 1
-
+        info = os.waitpid(pid, 0)
+        exit_code = os.WEXITSTATUS(info[1])
+        if exit_code != 0:
+            error += 1
+'''
 # print("Error num : ")
 # print(error)
 
