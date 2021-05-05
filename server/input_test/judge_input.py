5c6c412a078b84974d73c9b899aa932fe8b1042b
Dongw1126 <topblade71@naver.com>
2021-05-05 17:03:47 +0900

    input_test로 입력제어 폴더 생성

--- server/input_test/judge_input.py
+++ server/input_test/judge_input.py
@@ -1,5 +1,6 @@
 import os
 import sys
+sys.path.append(os.path.abspath('./'))
 from settings import *
 
 print("start input judge")
--- server/settings.py
+++ server/settings.py
@@ -1,4 +1,4 @@
-# TEST_PATH = "/home/dw/"
+# TEST_PATH = "/home/dw/server/"
 # HOME_PATH = TEST_PATH
 HOME_PATH = "/app/"
 
@@ -6,13 +6,13 @@ PYTHON_PATH = "/usr/bin/python3"
 GPP_PATH = "/usr/bin/g++"
 
 JUDGE_PATH = HOME_PATH + "judge.py"
-JUDGE_INPUT_PATH = HOME_PATH + "judge_input.py"
+JUDGE_INPUT_PATH = HOME_PATH + "input_test/judge_input.py"
 
 USR_CODE_PATH = HOME_PATH + "usr_code.cpp"
 OBJ_FILE_PATH = HOME_PATH + "a.out"
 COMPILE_LOG_PATH = HOME_PATH + "log.txt"
 
-INPUT_DIR_PATH = HOME_PATH + "res/input/"
+INPUT_DIR_PATH = HOME_PATH + "input_test/input/"
 OUTPUT_PATH = HOME_PATH + "output.txt"
 
 
