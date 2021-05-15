2c30312487dfa6aa745f8d7384d3e37f56048190
kddkdkk <dlwjddnrbb71@naver.com>
2021-05-16 02:57:34 +0900

    함수명, 매개변수 json dump

--- server/ast_dump/ast_dump.py
+++ server/ast_dump/ast_dump.py
@@ -1,23 +1,45 @@
 import sys
+import json
+from collections import OrderedDict
 import clang.cindex
 
+# libclang 파일 경로 바인딩
+# 로컬 디버깅 경로
+# clang.cindex.Config.set_library_path("/usr/lib/llvm-11/lib/")
+# 도커 빌드 경로
 clang.cindex.Config.set_library_file("/usr/lib/llvm-7/lib/libclang-7.so.1")
 
-def print_decl(node):
-    if (node.kind == clang.cindex.CursorKind.FUNCTION_DECL):
-        print ("\nfunc : ",end=" ")
-        print (node.spelling)
-    
-    if (node.kind == clang.cindex.CursorKind.PARM_DECL):
-        print ("parm : ",end=" ")
-        print (node.spelling,end=" , ")
+# 함수명과 파라미터 출력
+class parameter:
+    def __init__(self,path):
+        self.path = path
+        self.func = ""
+        self.data = OrderedDict()
 
-    for child in node.get_children():
-        if(str(sys.argv[1]) == str(child.location.file)):
-            print_decl(child)
-        else:
-            continue
+        index = clang.cindex.Index.create(False)
+        tu = index.parse(self.path)
+        self.print_decl(tu.cursor)
 
-index = clang.cindex.Index.create(False)
-tu = index.parse(sys.argv[1])
-print_decl(tu.cursor)
+    def print_json(self):
+        with open("parameter.json","w") as make_file:
+            json.dump(self.data, make_file, ensure_ascii=False, indent="\t")
+
+    def print_decl(self,node):
+        if (node.kind == clang.cindex.CursorKind.FUNCTION_DECL):
+            print ("\nfunc : ",end=" ")
+            print (node.spelling)
+            self.func = node.spelling
+            self.data[self.func] = {}
+            self.data[self.func]["type"] = node.type.spelling
+            self.data[self.func]["parameter"] = {}
+        
+        if (node.kind == clang.cindex.CursorKind.PARM_DECL):
+            print ("parm : ",end=" ")
+            print (node.spelling,end=" , ")
+            self.data[self.func]["parameter"][node.type.spelling] = node.spelling
+
+        for child in node.get_children():
+            if(str(self.path) == str(child.location.file)):
+                self.print_decl(child)
+            else:
+                continue
---
+++ server/ast_dump/get_ast.py
@@ -0,0 +1,4 @@
+import ast_dump
+
+param = ast_dump.parameter("main.cpp")
+param.print_json()
