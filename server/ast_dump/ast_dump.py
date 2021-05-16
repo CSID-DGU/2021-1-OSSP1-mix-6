424c22e743aecb7b5260e7bb022f698b04d5557a
kddkdkk <dlwjddnrbb71@naver.com>
2021-05-16 23:43:28 +0900

    ast 전체 덤프

--- server/ast_dump/ast_dump.py
+++ server/ast_dump/ast_dump.py
@@ -5,11 +5,31 @@ from clang import cindex
 
 # libclang 파일 경로 바인딩
 # 로컬 디버깅 경로
-# cindex.Config.set_library_path("/usr/lib/llvm-11/lib/")
+cindex.Config.set_library_path("/usr/lib/llvm-11/lib/")
 # 도커 빌드 경로
-cindex.Config.set_library_file("/usr/lib/llvm-7/lib/libclang-7.so.1")
+# cindex.Config.set_library_file("/usr/lib/llvm-7/lib/libclang-7.so.1")
 
-# 함수명과 파라미터 출력
+# ast 전체 출력
+class ast:
+    def __init__(self,path):
+        self.path = path
+        index = cindex.Index.create(False)
+        tu = index.parse(self.path)
+        self.traverse(tu.cursor)
+
+    def traverse(self,node, i=0):
+        print('\t' * i,end="")
+        print(node.kind, end="")
+        print(" : ",end="")
+        print(node.displayname,end="")
+        print("")
+        for child in node.get_children():
+            if(str(self.path) == str(child.location.file)):
+                self.traverse(child, i=i+1)
+            else:
+                continue
+
+# 함수명과 파라미터 출력 -> param.json
 class parameter:
     def __init__(self,path):
         self.path = path
@@ -18,13 +38,13 @@ class parameter:
 
         index = cindex.Index.create(False)
         tu = index.parse(self.path)
-        self.print_decl(tu.cursor)
+        self.set_param(tu.cursor)
 
     def print_json(self):
         with open("param.json","w") as make_file:
             json.dump(self.data, make_file, ensure_ascii=False, indent="\t")
 
-    def print_decl(self,node):
+    def set_param(self,node):
         if (node.kind == cindex.CursorKind.FUNCTION_DECL):
             self.func = node.spelling
             self.data[self.func] = {}
@@ -36,6 +56,35 @@ class parameter:
 
         for child in node.get_children():
             if(str(self.path) == str(child.location.file)):
-                self.print_decl(child)
+                self.set_param(child)
+            else:
+                continue
+
+# 모든 선언의 타입과 네이밍 출력 -> decl.json
+class declaration:
+    def __init__(self,path):
+        self.path = path
+        self.line = 0
+        self.data = OrderedDict()
+
+        index = cindex.Index.create(False)
+        tu = index.parse(self.path)
+        self.set_decl(tu.cursor)
+
+    def print_json(self):
+        with open("decl.json","w") as make_file:
+            json.dump(self.data, make_file, ensure_ascii=False, indent="\t")
+
+    def set_decl(self,node):
+        if (node.kind.is_declaration()):
+            self.line += 1
+            idx = "decl" + str(self.line)
+            self.data[idx] = {}
+            self.data[idx]["type"] = node.type.spelling
+            self.data[idx]["name"] = node.spelling
+
+        for child in node.get_children():
+            if(str(self.path) == str(child.location.file)):
+                self.set_decl(child)
             else:
                 continue
