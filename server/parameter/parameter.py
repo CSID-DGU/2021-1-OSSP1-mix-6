304cfee54b02060f64e06ddd12decd6252b60cd7
Limworld98 <38485221+Limworld98@users.noreply.github.com>
2021-06-12 14:22:31 +0900

    Update parameter.py

--- server/parameter/parameter.py
+++ server/parameter/parameter.py
@@ -50,7 +50,7 @@ class paraCounter:
         if self.Normal_count != 0:
             point = ((self.Normal_count-self.Over_count)/self.Normal_count) * 100
         f_out = open(PARAMETER_RESULT_PATH,'w')
-        f_out.write("PARAMETER POINT : " + str(point))
+        f_out.write("Parameter Score : " + str(point))
         f_out.close()
 
 
