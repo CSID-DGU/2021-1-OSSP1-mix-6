deb8476353034e005b7fec0a280276285be47155
Dongw1126 <topblade71@naver.com>
2021-06-17 14:04:24 +0900

    점수계산 수정

--- server/judge.py
+++ server/judge.py
@@ -40,58 +40,67 @@ else:
     f_total.close()
 
     # 입력 제어
-    pid_judge_input = os.fork()
-    if pid_judge_input == 0:
-        os.execl(PYTHON_PATH, "python3", JUDGE_INPUT_PATH, sys.argv[1])
-    os.waitpid(pid_judge_input, 0)
+    if usr_settings['inputAnalysisEnable']:
+        pid_judge_input = os.fork()
+        if pid_judge_input == 0:
+            os.execl(PYTHON_PATH, "python3", JUDGE_INPUT_PATH, sys.argv[1])
+        os.waitpid(pid_judge_input, 0)
 
     # 순환복잡도 분석
-    pid_complex = os.fork()
-    if pid_complex == 0:
-        os.execl(PYTHON_PATH, "python3", SCANNER_PATH)
-    os.waitpid(pid_complex, 0)
+    if usr_settings['complexityAnalysisEnable']:
+        pid_complex = os.fork()
+        if pid_complex == 0:
+            os.execl(PYTHON_PATH, "python3", SCANNER_PATH)
+        os.waitpid(pid_complex, 0)
 
     # 의존성 분석
-    pid_complex = os.fork()
-    if pid_complex == 0:
-        os.execl(PYTHON_PATH, "python3", DEPENDENCY_JUDGE_PATH)
-    os.waitpid(pid_complex, 0)
+    if usr_settings['dependenceAnalysisEnable']:
+        pid_complex = os.fork()
+        if pid_complex == 0:
+            os.execl(PYTHON_PATH, "python3", DEPENDENCY_JUDGE_PATH)
+        os.waitpid(pid_complex, 0)
 
     # 매개변수 분석
-    pid_complex = os.fork()
-    if pid_complex == 0:
-        os.execl(PYTHON_PATH, "python3", GET_PARAMETER)
-    os.waitpid(pid_complex, 0)
+    if usr_settings['parameterAnalysisEnable']:
+        pid_complex = os.fork()
+        if pid_complex == 0:
+            os.execl(PYTHON_PATH, "python3", GET_PARAMETER)
+        os.waitpid(pid_complex, 0)
 
     # 네이밍 분석
-    pid_complex = os.fork()
-    if pid_complex == 0:
-        os.execl(PYTHON_PATH, "python3", NAMING_JUDGE_PATH, sys.argv[1])
-    os.waitpid(pid_complex, 0)
+    if usr_settings['namingAnalysisEnable']:
+        pid_complex = os.fork()
+        if pid_complex == 0:
+            os.execl(PYTHON_PATH, "python3", NAMING_JUDGE_PATH, sys.argv[1])
+        os.waitpid(pid_complex, 0)
 
     # 중첩복잡도 분석
-    pid_repeat = os.fork()
-    if pid_repeat == 0:
-        os.execl(PYTHON_PATH, "python3", REPEAT_JUDGE_PATH)
-    os.waitpid(pid_repeat, 0)
+    if usr_settings['duplicationAnalysisEnable']:
+        pid_repeat = os.fork()
+        if pid_repeat == 0:
+            os.execl(PYTHON_PATH, "python3", REPEAT_JUDGE_PATH)
+        os.waitpid(pid_repeat, 0)
     
     # 실행시간 측정
-    pid_time = os.fork()
-    if pid_time == 0:
-        os.execl(PYTHON_PATH, "python3", GET_TIME, sys.argv[1])
-    os.waitpid(pid_time, 0)
+    if usr_settings['timeMemoryAnalysisEnable']:
+        pid_time = os.fork()
+        if pid_time == 0:
+            os.execl(PYTHON_PATH, "python3", GET_TIME, sys.argv[1])
+        os.waitpid(pid_time, 0)
 
     # 메모리 측정
-    pid_memory = os.fork()
-    if pid_memory == 0:
-        os.execl(PYTHON_PATH, "python3", GET_MEMORY, sys.argv[1])
-    os.waitpid(pid_memory, 0)
+    if usr_settings['timeMemoryAnalysisEnable']:
+        pid_memory = os.fork()
+        if pid_memory == 0:
+            os.execl(PYTHON_PATH, "python3", GET_MEMORY, sys.argv[1])
+        os.waitpid(pid_memory, 0)
 
     # 코드중복 분석
-    pid_complex = os.fork()
-    if pid_complex == 0:
-        os.execl(PYTHON_PATH, "python3", GET_DUPLICATION_PATH)
-    os.waitpid(pid_complex, 0)
+    if usr_settings['duplicationCodeAnalysisEnable']:
+        pid_complex = os.fork()
+        if pid_complex == 0:
+            os.execl(PYTHON_PATH, "python3", GET_DUPLICATION_PATH)
+        os.waitpid(pid_complex, 0)
 
     # 단순 실행 파트
     # fd = os.fdopen(f_output, "w")
